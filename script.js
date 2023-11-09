44642eae97ce0b1789db3bc6cf8186506d48cd7f
schismond <dadourachid720@gmail.com>
2023-11-09 16:49:54 +0100

    fixed js function

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ let hiddenContent = document.querySelectorAll(".hidden-content");
 let questions = document.querySelectorAll(".question");
 
 for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
-    if (window.getComputedStyle(hiddenContent[i]).display === "none") {
-      hiddenContent[i].style.display = "block";
-      questions[i].style.fontWeight = "bold";
-      questions[i].style.color = "black";
+  buttons[i].addEventListener("click", function() { displayText(i); });
+  questions[i].addEventListener("click", function() { displayText(i); });
+}
 
-      buttons[i].style.transform = "rotate(180deg)";
-    } else {
-      hiddenContent[i].style.display = "none";
-      questions[i].style.fontWeight = "normal";
-      buttons[i].style.transform = "rotate(0deg)";
-    }
-  });
+function displayText(i) {
+  if (window.getComputedStyle(hiddenContent[i]).display === "none") {
+    hiddenContent[i].style.display = "block";
+    questions[i].style.fontWeight = "bold";
+    questions[i].style.color = "black";
+
+    buttons[i].style.transform = "rotate(180deg)";
+  } else {
+    hiddenContent[i].style.display = "none";
+    questions[i].style.fontWeight = "normal";
+    buttons[i].style.transform = "rotate(0deg)";
+  }
 }
